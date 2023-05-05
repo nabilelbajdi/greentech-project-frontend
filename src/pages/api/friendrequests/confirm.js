5f3077a15f55690d376fecc9e30ec6d135b4d936
Christoffer <christoffer@christoffergylin.com>
2023-05-05 17:18:25 +0200

    Added friend requests and fixed problem with prisma client

---
+++ src/pages/api/friendrequests/confirm.js
@@ -0,0 +1,122 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import prisma from "../../../../server/db/prisma";
+
+const confirm = async (req, res) => {
+
+    try {
+
+        const session = await getServerSession(req, res, authOptions);
+
+        if (session) {
+            const prismaUser = await prisma.user.findUnique({
+                where: { email: session.user.email }
+            })
+
+            if (!prismaUser) {
+                res.status(401).json({ error: 'Unauthorized' });
+                return;
+            }
+
+            const { method } = req;
+
+            switch (method) {
+                case 'POST':
+                    const { userId } = req.body;
+
+                    if (!prismaUser.id === userId) {
+                        res.status(403).end('You can´t confirm a friend request from yourself.');
+                        return;
+                    };
+
+                    const checkUser = await prisma.user.findUnique({
+                        where: {
+                            id: userId
+                        },
+                        include: {
+                            friendRequests: true,
+                            friendRequestsSent: true,
+                            friends: true,
+                        }
+                    })
+
+                    if (!checkUser) {
+                        receiverUser.status(404).end('User does not exist.');
+                        return;
+                    }
+
+                    let allOk = false;
+
+                    checkUser.friendRequestsSent.forEach(user => {
+                        if (user.id === prismaUser.id) allOk = true;
+                    });
+                    checkUser.friends.forEach(user => {
+                        if (user.id === prismaUser.id) allOk = false;
+                    });
+
+                    if (!allOk) {
+
+                        res.status(403).end('Can not create friend relation.');
+                        return;
+
+                    }
+
+                    await prisma.user.update({
+
+                        where: {
+                            id: userId,
+                        },
+                        data: {
+                            friendRequestsSent: {
+                                disconnect: {
+                                    id: prismaUser.id,
+                                },
+                            },
+                            friends: {
+                                connect: {
+                                    id: prismaUser.id,
+                                },
+                            },
+                        },
+                    });
+
+                    await prisma.user.update({
+
+                        where: {
+                            id: prismaUser.id,
+                        },
+                        data: {
+                            friendRequests: {
+                                disconnect: {
+                                    id: userId,
+                                },
+                            },
+                            friends: {
+                                connect: {
+                                    id: userId,
+                                },
+                            },
+                        },
+                    });
+
+                    res.status(200).send();
+                    break;
+
+                default:
+                    res.status(405).send();
+            }
+
+        } else {
+
+            res.status(403).end('You must be signed in to do this.')
+
+        }
+
+    } catch (e) {
+
+        res.status(500).send({ error: e.message });
+
+    }
+}
+
+export default confirm;
