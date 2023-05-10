5bcfc73c99a28151212dd82c91bef4a4dbe52472
ChasAcademy-Fernando <115954598+ChasAcademy-Fernando@users.noreply.github.com>
2023-05-11 00:20:36 +0200

    Status Box
    DO NOT MERGE THIS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,40 @@
 import { Inter } from 'next/font/google';
 import Feed from '@/components/Feed';
-import getProps from '@/utils/getProps';
-import Header from '@/components/Header';
+
 import Head from "next/head";
 import Sidebar from '@/components/Sidebar';
-export const getServerSideProps = getProps;
+import HomeFeed from '@/components/HomeFeed';
+import { PrismaClient } from '@prisma/client';
+
+
 
 const inter = Inter({ subsets: ['latin'] });
 
-const Home = () => {
+const Home = ({posts}) => {
   return (
-   <div>
+    <div>
+    <div className='h-screen bg-gray-100 overflow-hidden'>
     <Head>
     <title>TreeHouse</title>
     </Head>
-    <main> 
-      <Header/>
+    <main className='flex'>
       <Sidebar/>
-      <Feed />
+      <HomeFeed/>
+      {/* <HomeFeed posts ={posts}/> */}
+      
     </main>
+    </div>
+    <Feed />
     </div>
   );
 };
+export async function getStaticProps() {
+  const prisma = new PrismaClient()
+  const posts = await prisma.post.findMany()
+
+  return {
+    props : {posts: JSON.parse(JSON.stringify(posts))}
+  }
+}
 
 export default Home;
---
+++ src/pages/profile.js
@@ -0,0 +1,14 @@
+import HomeFeed from "@/components/HomeFeed";
+import Sidebar from "@/components/Sidebar";
+
+const Profile = () => {
+    return (  
+        <div>
+        <Sidebar/>
+        
+        </div>
+    );
+}
+
+ 
+export default Profile;
