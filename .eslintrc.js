d7cc0eefc0e9b865a86131a35c2d423bfb80fb7c
Nabil El Bajdi <113439816+nabilelbajdi@users.noreply.github.com>
2023-05-11 16:47:54 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// .eslintrc.js example
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: ["error", "always"],
+  },
+};
