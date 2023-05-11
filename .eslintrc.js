b6c298e1ccd600e01fedcbad1f52bfb0c4e20650
Nabil El Bajdi <113439816+nabilelbajdi@users.noreply.github.com>
2023-05-11 17:48:35 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   env: {
-    es6: true,
-    node: true,
+    browser: true,
+    es2021: true,
+    node: true
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
-  plugins: ["prettier"],
   rules: {
-    "prettier/prettier": "error",
+    semi: ["error", "always"],
   },
 };
