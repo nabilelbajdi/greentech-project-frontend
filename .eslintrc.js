70a5147f15422f0aaef2b801536a918a319619dd
Nabil El Bajdi <113439816+nabilelbajdi@users.noreply.github.com>
2023-05-22 11:53:46 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
+  extends: ['next'],
   rules: {
-    'placeholder-rule': 'warn'
-  }
+    // ...
+  },
 };
