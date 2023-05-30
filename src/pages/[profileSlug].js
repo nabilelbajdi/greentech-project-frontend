bea06a95867b4d0060ad226611e6560379843206
ChasAcademy-Linnea-Svensson <113734152+ChasAcademy-Linnea-Svensson@users.noreply.github.com>
2023-05-30 12:48:33 +0200

    added: profile page

---
+++ src/pages/[profileSlug].js
@@ -0,0 +1,36 @@
+import Posts from '@/components/Posts';
+import ProfileCard from '@/components/ProfileCard';
+import getProfilePageProps from '@/utils/getProfilePageProps';
+import Link from 'next/link';
+export const getServerSideProps = getProfilePageProps;
+
+const EventPage = (props) => {
+  return (
+    <div className='w-full'>
+      <main className='flex flex-col'>
+        {props.user ? (
+          <div className='flex px-10'>
+            <div className='mr-20'>
+              <h2 className='border-b-2 border-black'>Evenenemang</h2>
+              {props.user.eventsCreated.map((event) => (
+                <Link key={event.id} href={`/events/${event.id}`}>
+                  <div className='flex items-center justify-center bg-white rounded-xl h-20 w-60 my-4'>
+                    {event.name}
+                  </div>
+                </Link>
+              ))}
+            </div>
+            <div>
+              <ProfileCard user={props.user} />
+              <Posts posts={props.user.posts} />
+            </div>
+          </div>
+        ) : (
+          <h1>Användaren hittades inte</h1>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default EventPage;
