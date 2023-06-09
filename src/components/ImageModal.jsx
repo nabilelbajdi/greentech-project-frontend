e082de1ca49f50f1123b6c637e74737f35c605ef
Robin Bergstrand <robin.bergstrand@chasacademy.se>
2023-06-09 22:21:43 +0200

    fixed modal arrows being to tall

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -31,7 +31,7 @@ const ImageModal = ({ images, idx, closeModal, next, previous }) => {
   return (
     <div className='fixed inset-0 z-[51] bg-black bg-opacity-60 flex justify-center items-center'>
       <div className='absolute inset-0 z-0' onClick={() => closeModal()} />
-      <div className='z-10 flex items-center gap-2'>
+      <div className='z-10 flex items-center justify-center gap-2 '>
         <button
           className=' z-[100] cursor-pointer w-10 h-10 rounded-full text-white hover:text-gray-400'
           onClick={previousImage}
