86939ad01af7ae12069385566bc0ed5e380ccba7
ChasAcademy-Linnea-Svensson <113734152+ChasAcademy-Linnea-Svensson@users.noreply.github.com>
2023-06-10 12:28:20 +0200

    changed small design issues

--- src/components/ItemPreview.jsx
+++ src/components/ItemPreview.jsx
@@ -27,7 +27,7 @@ const ItemPreview = ({ item, type }) => {
         />
       </div>
       <div className='flex flex-col gap-2 w-full p-2'>
-        <p>{item.name}</p>
+        <p className='font-bold'>{item.name}</p>
         {item.condition && <p>{item.condition}</p>}
       </div>
     </div>
